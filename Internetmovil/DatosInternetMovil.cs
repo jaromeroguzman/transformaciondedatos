ed9d2de36b39ac1aa20e9f81ebe3e849ad2b543e
iteraciones_suma_registros <carlo@LAPTOP-M667DM4Q>
2019-07-25 21:01:26 -0500

    json

---
+++ Internetmovil/DatosInternetMovil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CsvHelper.Configuration.Attributes;
+
+namespace Internetmovil
+{
+    class DatosInternetMovil
+    {
+        [Name("PROVEEDOR")]
+        public string Proveedor { get; set; }
+        [Name("SUSCRIPTORES_2015_2T")]
+        public int Suscriptores_2015_2T { get; set; }
+        [Name("SUSCRIPTORES_2016_1T")]
+        public int Suscriptores_2016_1T { get; set; }
+        [Name("POBLACIONS_2016_1T")]
+        public int Poblacion_2016_1T { get; set; }
+        [Name("POBLACION_2015_2T")]
+        public int Poblacion_2015_2T { get; set; }
+
+    }
+}
---
+++ Internetmovil/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CsvHelper;
+using System.IO;
+using System.Linq;
+
+namespace Internetmovil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ruta = @"C:\Users\carlo\Downloads\Comparativo_Segundo_Trimestre_vs_Primer_Trimestre_2016.csv";
+            var archivo = File.OpenText(ruta);
+            var csv = new CsvReader(archivo);
+            csv.Configuration.Delimiter = ",";
+            var registros = csv.GetRecords<DatosInternetMovil>().ToList();
+        }
+    }
+}
